aeb49b94d036d4ee8e883f65a65dd0bec49f9a9c
a.k.a fl1p <bishu.hover@gmail.com>
2025-12-09 11:29:14 +0800

    post slug link

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,7 +1,11 @@
 import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 
-export default async function Post({ params }: { params: Promise<{ id: string }> }) {
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
   const post = await prisma.post.findUnique({
     where: { id: parseInt(id) },
@@ -19,7 +23,7 @@ export default async function Post({ params }: { params: Promise<{ id: string }>
       <article className="max-w-2xl space-y-4 font-[family-name:var(--font-geist-sans)]">
         <h1 className="text-4xl font-bold mb-8 text-[#333333]">{post.title}</h1>
         <p className="text-gray-600 text-center">by {post.author.name}</p>
-        <div className="prose prose-gray mt-8">
+        <div className="prose prose-gray mt-8 text-black">
           {post.content || "No content available."}
         </div>
       </article>
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 export default async function Posts() {
   const posts = await prisma.post.findMany({
@@ -12,13 +13,15 @@ export default async function Posts() {
       <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)]">
         Posts
       </h1>
-      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
+      <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4 ">
         {posts.map((post) => (
           <li key={post.id}>
-            <span className="font-semibold">{post.title}</span>
-            <span className="text-sm text-gray-600 ml-2">
-              by {post.author.name}
-            </span>
+            <Link href={`/posts/${post.id}`}>
+              <span className="font-semibold">{post.title}</span>
+              <span className="text-sm text-gray-600 ml-2">
+                by {post.author.name}
+              </span>
+            </Link>
           </li>
         ))}
       </ul>
